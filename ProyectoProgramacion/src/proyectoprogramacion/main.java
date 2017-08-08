f91651a0dbbf9cc5692c357522dce73a686bea76
diegogimenez04 <d.gimenez0101@gmail.com>
2017-08-08 13:54:47 -0300

    Borro proyecto viejo

--- ProyectoProgramacion/src/proyectoprogramacion/main.java
+++
@@ -1,7 +0,0 @@
-package proyectoprogramacion;
-public class main {
-    public static void main(String[] args){
-        VentanaInicio ejecutar = new VentanaInicio();
-        ejecutar.setVisible(true);
-    }
-}
