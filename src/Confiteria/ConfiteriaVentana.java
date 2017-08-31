e388f0b264a9313acbc728400028acddb75a9df1
Diego Gimenez <d.gimenez0101@gmail.com>
2017-08-31 11:51:47 -0300

    Añadi dos arraylist a la clase confiteria, falta setearlas en el panel

--- src/Confiteria/ConfiteriaVentana.java
+++ src/Confiteria/ConfiteriaVentana.java
@@ -8,7 +8,7 @@ package Confiteria;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
+import javax.swing.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -18,26 +18,37 @@ import javax.swing.JPanel;
  */
 public class ConfiteriaVentana extends JFrame implements ActionListener{
     
-    Confiteria confi;
-    
     JButton save;
     JButton cancel;
     JButton cuadrado;
     
+    JComboBox bebida;
+    JComboBox comida;
+    
+    JLabel total;
+    
+    //Creamos el objeto de confiteria
+    Confiteria confiteria = new Confiteria();
+    
     public ConfiteriaVentana(){
+        //Definimos caracteristicas de la ventana
         setSize(400,400);
         setLocation(300,500);
+        
+        bebida.setModel(new DefaultComboBoxModel(confiteria.listabebida.toArray()));
+        comida.setModel(new DefaultComboBoxModel(confiteria.listacomida.toArray()));
         
         JPanel panel = new JPanel();
         
         this.add(panel);
         
-        
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        
+        if (ae.getSource().equals(save)){
+            
+        }
     }
     
 }
